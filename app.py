723e25ce509298e3f83d04e7b95070c221e97949
Kbliaya <kbliaya@csu.fullerton.edu>
2023-04-10 00:02:23 -0700

    added uploads folder

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ import os
 app = Flask(__name__)
 cors = CORS(app, resources={r'/*': {'origins': '*'}})
 
-conn = pymysql.connect(
-    host='localhost',
-    user='root',
-    password = 'insert_password',
-    db='db_name',
-    cursorclass=pymysql.cursors.DictCursor
-)
-cur = conn.cursor()
+# conn = pymysql.connect(
+#     host='localhost',
+#     user='root',
+#     password = 'insert_password',
+#     db='insert_db_name',
+#     cursorclass=pymysql.cursors.DictCursor
+# )
+# cur = conn.cursor()
 
 @app.errorhandler(400)
 def bad_request(e):
@@ -40,7 +40,7 @@ app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
 app.config['UPLOAD_PATH'] = 'uploads'
 
-@app.route('/', methods=['POST'])
+@app.route('/upload_file', methods=['POST'])
 def upload_file():
     uploaded_file = request.files['file']
     filename = secure_filename(uploaded_file.filename)
